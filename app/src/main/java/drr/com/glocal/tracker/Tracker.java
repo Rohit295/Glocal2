aa92da1e5e8a5578f4daa2343a63354e0e145ea2
Rohit295 <rohit295@gmail.com>
2014-10-30 14:59:26 +0530

    Interim Updates on these classes

--- app/src/main/java/drr/com/glocal/tracker/Tracker.java
+++ app/src/main/java/drr/com/glocal/tracker/Tracker.java
@@ -1,31 +1,30 @@
 package drr.com.glocal.tracker;
 
 import android.app.Activity;
-import android.content.Context;
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.google.android.gms.maps.MapView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import drr.com.glocal.R;
 
 public class Tracker extends Activity {
-
-    MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker2);
 
-        // MapView requires the lifecycle events to be passed along
-        mMapView.onCreate(savedInstanceState);
-
-        // actually we want the special fragment being the
-        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new TrackerFragment())
+                    .commit();
+        }
     }
 
 
@@ -46,5 +45,37 @@ public class Tracker extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class TrackerFragment extends Fragment implements View.OnClickListener {
+
+        public TrackerFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_tracker2, container, false);
+
+            // Add a Button listener to start location tracking
+            Button startButton = (Button) rootView.findViewById(R.id.btn_start_tracking);
+            startButton.setOnClickListener(this);
+
+            return rootView;
+        }
+
+        @Override
+        public void onClick(View view) {
+            // If the user has pressed the Start Button
+            Intent locationTrackingIntent = new Intent(getActivity(), LocationTrackerService.class);
+            if (view.getId() == R.id.btn_start_tracking) {
+                getActivity().startService(locationTrackingIntent);
+            } else if (view.getId() == R.id.btn_stop_tracking) {
+                getActivity().stopService(locationTrackingIntent);
+            }
+        }
     }
 }
