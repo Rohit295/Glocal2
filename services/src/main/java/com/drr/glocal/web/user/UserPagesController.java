9ffa9490f2451749ca93a598671f15d4e6197334
Rohit295 <rohit295@gmail.com>
2015-03-06 20:24:01 +0530

    Initial Version still - socket methods added, Google Maps control added

--- services/src/main/java/com/drr/glocal/web/user/UserPagesController.java
+++ services/src/main/java/com/drr/glocal/web/user/UserPagesController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.appengine.api.channel.ChannelService;
+import com.google.appengine.api.channel.ChannelServiceFactory;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 /**
  * Master Controller for all User Pages in WIX
  * @author rohitman
@@ -14,7 +19,18 @@ public class UserPagesController {
 
     @RequestMapping("/user/console")
     public ModelAndView getUserConsole() {
-        return new ModelAndView("user/console");
+    	
+    	// TODO Create a proper channel for this user. Add that to the track(s) that the user is following
+    	
+    	// get the current user ID, to create a Channel 
+		UserService userService = UserServiceFactory.getUserService();
+		String userID = "rohit295@gmail.com";
+		//String userID = userService.getCurrentUser().getUserId();
+		
+		ChannelService channelService = ChannelServiceFactory.getChannelService();
+		String channelID = channelService.createChannel(userID);
+    			
+        return new ModelAndView("user/console", "channelID", channelID);
     }
 
 }
