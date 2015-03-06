77dfd2371cb1d23ea4e18fa61109282f41f6e613
Rohit295 <rohit295@gmail.com>
2015-03-06 18:01:19 +0530

    New Controller for all WIX User actions

---
+++ services/src/main/java/com/drr/glocal/web/tracker/UserPagesController.java
@@ -0,0 +1,20 @@
+package com.drr.glocal.web.tracker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Master Controller for all User Pages in WIX
+ * @author rohitman
+ *
+ */
+@Controller
+public class UserPagesController {
+
+    @RequestMapping("/user/console")
+    public ModelAndView getUserConsole() {
+        return new ModelAndView("user/console");
+    }
+
+}
